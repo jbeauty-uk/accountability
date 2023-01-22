51f0446f473b3fec973b7efd6464c98afca18a0a
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2023-01-22 13:28:39 +0000

    sentry monitoring

--- frontend/sentry.server.config.js
+++ frontend/sentry.server.config.js
@@ -2,12 +2,12 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://173c94b86c184b26ab528a3d901c0009@o4504548354752512.ingest.sentry.io/4504548358684672',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
