51e995187bb15513075fa777306d95908024c34d
Dainis <44690511+dgrinbergs@users.noreply.github.com>
2023-01-22 17:42:42 +0000

    backend implementation

--- src/main/java/uk/jbeauty/accountability/configuration/BeanConfiguration.java
+++ src/main/java/uk/jbeauty/accountability/configuration/BeanConfiguration.java
@@ -3,7 +3,9 @@ package uk.jbeauty.accountability.configuration;
 import graphql.scalars.ExtendedScalars;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Clock;
@@ -26,6 +28,21 @@ class BeanConfiguration {
     return wiringBuilder -> wiringBuilder
         .scalar(ExtendedScalars.Date)
         .scalar(ExtendedScalars.GraphQLLong);
+  }
+
+  @Profile("prod")
+  @Bean("authorizeExchangeSpec")
+  ServerHttpSecurity.AuthorizeExchangeSpec authorizeExchangeSpecProd(ServerHttpSecurity http) {
+    return http.authorizeExchange()
+        .anyExchange().authenticated();
+  }
+
+  @Bean("authorizeExchangeSpec")
+  @Profile("dev")
+  ServerHttpSecurity.AuthorizeExchangeSpec authorizeExchangeSpecDev(ServerHttpSecurity http) {
+    return http.authorizeExchange()
+        .pathMatchers("/graphiql", "/graphql").permitAll()
+        .anyExchange().authenticated();
   }
 
 }
